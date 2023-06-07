007fc0650d2085abda44349a5c00bb5d43fa4546
Like_Frost <1342230493@qq.com>
2023-06-07 17:31:04 +0800

    fix: change basic doc

--- examples/docs/src/hooks/useI18n.ts
+++ examples/docs/src/hooks/useI18n.ts
@@ -2,7 +2,7 @@ import { createContext, useContext } from 'react';
 import { template, templateSettings } from 'lodash-es';
 
 export const LocaleContext = createContext<'en' | 'zh'>('en');
-export const useLocale= () => useContext(LocaleContext);
+export const useLocale = () => useContext(LocaleContext);
 
 templateSettings.interpolate = /{([\s\S]+?)}/g;
 export const compiled = (str: string, params: Record<string, string>) => template(str)(params);
@@ -10,6 +10,6 @@ export const compiled = (str: string, params: Record<string, string>) => templat
 const useI18n = <T extends Record<'en' | 'zh', Record<string, string>>>(transitions: T): T['en' | 'zh'] => {
     const locale = useContext(LocaleContext);
     return transitions[locale];
-}
+};
 
 export default useI18n;
